4d50feb9656e17fe0d2f227a8ced47e63adc4ff7
Rafael Reyes <141748653+RafaelReyes0816@users.noreply.github.com>
2023-11-29 21:42:49 -0400

    Almacenamiento de nombres

---
+++ Lista-almacenamiento/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Lista_almacenamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListNombres = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(320, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lista de Nombres";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ListNombres
+            // 
+            this.ListNombres.FormattingEnabled = true;
+            this.ListNombres.Location = new System.Drawing.Point(341, 126);
+            this.ListNombres.Name = "ListNombres";
+            this.ListNombres.Size = new System.Drawing.Size(120, 95);
+            this.ListNombres.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(358, 237);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(358, 282);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Location = new System.Drawing.Point(250, 93);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(44, 13);
+            this.Nombre.TabIndex = 4;
+            this.Nombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(325, 79);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(143, 27);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.ListNombres);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ListNombres;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ Lista-almacenamiento/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista_almacenamiento
+{
+        public partial class Form1 : Form
+        {
+            private List<string> listaEstudiantes = new List<string>();
+            public Form1()
+            {
+
+                InitializeComponent();
+            }
+            private void label1_Click(object sender, EventArgs e)
+            {
+
+            }
+            private void btnAdd_Click(object sender, EventArgs e)
+            {
+            // Obtener el nombre ingresado
+            string nombre = txtNombre.Text.Trim();
+
+            // Verificar si el nombre no está vacío
+            if (!string.IsNullOrEmpty(nombre))
+            {
+                // Verificar si el nombre ya está en la lista
+                if (!NombreEnLista(nombre))
+                {
+                    // Agregar el nombre a la lista
+                    listaEstudiantes.Add(nombre);
+
+                    // Limpiar y volver a llenar el ListBox
+                    ListNombres.Items.Clear();
+                    foreach (string estudiante in listaEstudiantes)
+                    {
+                        ListNombres.Items.Add(estudiante);
+                    }
+
+                    // Limpiar la caja de texto
+                    txtNombre.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("El nombre ya está en la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un nombre.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+            private void btnLimpiar_Click(object sender, EventArgs e)
+            {
+                // Limpiar la lista y el ListBox
+                listaEstudiantes.Clear();
+                ListNombres.Items.Clear();
+        }
+        private bool NombreEnLista(string nombre)
+        {
+            // Verificar si el nombre ya está en la lista
+            return listaEstudiantes.Contains(nombre);
+        }
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+            {
+                
+            }
+        }
+    }
